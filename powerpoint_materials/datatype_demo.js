b4905e6170d90f9b9bbe396fc29436acc09f9b0d
seanchok <seanchok@gmail.com>
2019-02-01 13:41:26 +0800

    prepare powerpoint materials

---
+++ powerpoint_materials/datatype_demo.js
@@ -0,0 +1,15 @@
+let utkarsh = {
+  name: "Utkarsh Goel",
+  age: 21,
+  goesToGymRegularly: false,
+  favColours: ["blue", "red", "black"]
+}
+let sean = {
+  name: "Sean Chok",
+  age: 20,
+  goesToGymRegularly: true,
+  favColours: ["blue", "black", "white"]
+}
+
+let rctechMembers = [utkarsh, sean]
+console.log(rctechMembers[1].favColours[1])
